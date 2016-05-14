b14b507990c607e7f5059e98c88a8f12d3548790
한기훈 <kordreamfollower@gmail.com>
2016-05-15 04:39:07 +0900

    서버상에서 모바일 리디렉트 하던거 제거, 반응형으로 프론트엔드 재설계 작업 시작.

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,9 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var MobileDetect = require('mobile-detect');
 
 router.get('*', function(req, res, next) {
-  var md = new MobileDetect(req.headers['user-agent']);
-
-  if(md.phone()) {
-    res.sendFile(path.join(__dirname, '../', 'public', 'mobile.html'));
-  }
-  else if(md.tablet()) {
-    res.sendFile(path.join(__dirname, '../', 'public', 'desktop.html'));
-  }
-  else {
-    res.sendFile(path.join(__dirname, '../', 'public', 'desktop.html'));
-  }
+  res.sendFile(path.join(__dirname, '../', 'public', 'index.html'));
 });
 
 module.exports = router;
